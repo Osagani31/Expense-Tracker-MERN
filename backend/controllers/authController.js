4a6a3e0aa954055b679b4e72334867eae1ba0594
Osagani31 <naonperera@students.nsbm.ac.lk>
2025-12-29 17:01:05 +0530

    Enhance authController with user registration and login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,13 +5,13 @@ const jwt = require('jsonwebtoken');
 // Generate JWT Token
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '1h',
+        expiresIn: '24h',
     });
 }
 
 //Register User
 exports.registerUser = async (req, res) => {
-  const { fullName, email, password } = req.body;
+  const { fullName, email, password, profileImageUrl } = req.body;
 
   //Validation check for missing fields
     if (!fullName || !email || !password) {
@@ -25,7 +25,7 @@ exports.registerUser = async (req, res) => {
         }
 
         // Create the user
-        const user = await User.create({ fullName, email, password });
+        const user = await User.create({ fullName, email, password, profileImageUrl });
 
         res.status(201).json({
             id: user._id,
@@ -43,10 +43,42 @@ exports.registerUser = async (req, res) => {
 
 //Login User
 exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    //Validation check for missing fields
+    if (!email || !password) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+    try {
+        //Check if user exists
+        const user = await User.findOne({ email });
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        res.status(200).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    } catch (err) {
+        res.status(500).json({ message: " Error logging in", error: err.message });
+    }
 
 
-    
 };
 
-//Register User
-exports.getUserInfo = async (req, res) => {};
+// Get User Info
+exports.getUserInfo = async (req, res) => {
+ try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+ } catch (err) {
+    res.status(500).json({ message: " Error registering user info", error: err.message });
+ }
+};
