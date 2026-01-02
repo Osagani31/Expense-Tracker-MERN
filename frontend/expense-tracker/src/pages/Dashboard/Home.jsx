2dacc41a2e30036be2a3da5add9dcfda02b4d236
Osagani31 <naonperera@students.nsbm.ac.lk>
2026-01-02 10:46:53 +0530

    Implement dashboard data fetching in Home component
    Fetch and display dashboard data with loading state.

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+import { useUserAuth } from '../../hooks/useUserAuth';
+import { API_PATHS } from '../../utils/apiPaths';
+import axiosInstance from '../../utils/axiosInstance';
+
 
 function Home() {
+  useUserAuth();
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    const fetchDashboardData = async () => {
+      setLoading(true);
+      try {
+        const response = await axiosInstance.get(
+          API_PATHS.DASHBOARD.GET_DATA
+        );
+
+        if (response?.data) {
+          setDashboardData(response.data);
+        }
+      } catch (error) {
+        console.error("Failed to fetch dashboard data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDashboardData();
+  }, []);
+
   return (
-    <div>
-      Home
-      
-    </div>
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Balance "
+            value={addThousandSeparators(dashboardData?.totalBalance || 0)}
+             color="bg-primary"
+          />
+         </div>
+        
+      </div>
+    </DashboardLayout>
   );
 }
 
