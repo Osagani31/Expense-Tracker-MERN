85876fd8c059af675a928f0f5d8925f1542bfb95
Osagani31 <naonperera@students.nsbm.ac.lk>
2026-01-03 17:38:56 +0530

    Change server port from 5000 to 8000

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use(
 
 
 
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 8000;
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
